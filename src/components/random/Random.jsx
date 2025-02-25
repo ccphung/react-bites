0782fc7e835cee1514ebf1a1e4d52da5e8fee3e6
Cédric Phung <cedric3011@outlook.fr>
2025-02-25 10:54:17 +0100

    create random component

---
+++ src/components/random/Random.jsx
@@ -0,0 +1,47 @@
+import styles from "./Random.module.css";
+import { ShuffleOutlined } from "@mui/icons-material";
+import { Button } from "@mui/material";
+import { useEffect, useState } from "react";
+
+function Random({ selectedId, setSelectedId }) {
+  const [clicked, setClicked] = useState(false);
+
+  useEffect(
+    function () {
+      if (!clicked) return;
+      async function getRandom() {
+        try {
+          const res = await fetch(
+            "https://www.themealdb.com/api/json/v1/1/random.php"
+          );
+          const data = await res.json();
+          setSelectedId(data.meals[0].idMeal);
+        } catch (err) {
+          console.log(err.message);
+        } finally {
+          setClicked(false);
+        }
+      }
+      getRandom();
+    },
+    [clicked]
+  );
+
+  return (
+    <div className={styles.container}>
+      <Button
+        variant="contained"
+        onClick={() => setClicked(true)}
+        sx={{
+          backgroundColor: "#008771",
+        }}
+        className={styles.button}
+      >
+        <ShuffleOutlined className={styles.icon} />
+        Random recipe
+      </Button>
+    </div>
+  );
+}
+
+export default Random;
