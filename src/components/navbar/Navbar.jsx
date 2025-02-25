067e363fae42ac2eb71a45ac7653cbcecb867c16
Cédric Phung <cedric3011@outlook.fr>
2025-02-25 10:54:33 +0100

    remove favorite button

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,14 @@
 import Logo from "../logo/Logo";
+import Random from "../random/Random";
 import SearchBar from "../searchbar/SearchBar";
 import styles from "./Navbar.module.css";
 
-function Navbar({ setQuery }) {
+function Navbar({ setQuery, selectedId, setSelectedId }) {
   return (
     <div className={styles.nav}>
       <Logo />
       <SearchBar setQuery={setQuery} />
+      <Random selectedId={selectedId} setSelectedId={setSelectedId} />
     </div>
   );
 }
