bd308aeea00ef1c636450ad3a06da939ac638746
Cédric Phung <cedric3011@outlook.fr>
2025-02-25 10:53:06 +0100

    move state to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import { useState } from "react";
+import Navbar from "./components/navbar/Navbar";
+import Homepage from "./pages/homepage/Homepage";
+
 function App() {
-  return <></>;
+  const [query, setQuery] = useState("");
+  const [showFavorites, setShowFavorites] = useState(false);
+  const [selectedId, setSelectedId] = useState(null);
+  return (
+    <>
+      <Navbar
+        setQuery={setQuery}
+        selectedId={selectedId}
+        setSelectedId={setSelectedId}
+      />
+      <Homepage
+        query={query}
+        showFavorites={showFavorites}
+        setShowFavorites={setShowFavorites}
+        selectedId={selectedId}
+        setSelectedId={setSelectedId}
+      />
+    </>
+  );
 }
 
 export default App;
